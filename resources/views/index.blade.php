b6d68d287025341a7f26f382ea43b5e4cec7fc27
hasan <hassanhitleap1@gmail.com>
2018-09-24 11:33:42 +0300

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,16 +7,16 @@
                    <button type="button" class="btn btn-primary btn-lg float-right button-save" >Save</button>
                    <img class="card-img-top" src="{{asset($product->image_path)}}" alt="Card image cap">
                    <div class="card-body">
-                      <h4 class="float-right"><i class="fas fa-dollar-sign"></i> {{floor($product->price)}}{{ltrim(($product->price - floor($product->price)),"0.")}}</h4>
+                      <h4 class="float-right"><i class="fas fa-dollar-sign"></i>{{($product->price)}}</h4>
                       <h4 class="card-title">{{$product->name}}</h4>
-                      <p class="card-text">{{ substr($product->description,0,100)}}...</p>
+                      <p class="card-text">{{ substr($product->description,0,400)}}</p>
                    </div>
                    <div class="card-footer">
                       <i class="far fa-heart"></i> <small class="text-muted">love this </small>
                       <a href="{{$product->link}}" class="btn btn-primary btn-sm float-right " >Check out</a>
                    </div>
                 </div>
-             </div>
+        </div>
 @endforeach
 
 <script type="text/javascript" src="{{asset('js/index.js')}}"></script>
