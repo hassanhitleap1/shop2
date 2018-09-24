7ba7edc8fa759960b1ab0befef56ddc5d137a91a
hasan <hassanhitleap1@gmail.com>
2018-09-24 17:31:31 +0300

    load model

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,15 +25,7 @@
       <div class="modal fade" id="test" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
                <div class="modal-content">
-                  <div class="modal-header">
-                     <h5 class="modal-title" id="exampleModalLongTitle">content</h5>
-                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                     <span aria-hidden="true">&times;</span>
-                     </button>
-                  </div>
-                  <div class="modal-body">
-               
-                  </div>
+           
                </div>
             </div>
          </div>
@@ -51,7 +43,7 @@
             $("#loginClick").click(function (e) { 
                 e.preventDefault();
          
-            $( ".modal-body" ).load( "user/login .modal-body" , function() {
+            $( ".modal-content" ).load( "user/login" , function() {
                     $('#test').modal('show');   
                 });
                //$('#loginModal').modal('show');
