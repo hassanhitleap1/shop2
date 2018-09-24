03434cc575f8ded86bce2a49858d11daa62a4ad1
hasan <hassanhitleap1@gmail.com>
2018-09-24 11:05:55 +0300

    footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -4,19 +4,22 @@
                 <div class="col-xs-12 col-sm-4 col-md-4">
                     <h5>Menu</h5>
                     <ul class="list-unstyled quick-links">
-                        <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                        <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Connect us</a></li>
-                        <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-                        <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Show your products</a></li>
+                        <li><a href="{{url('/home')}}"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                        <li><a href="{{url('/contact')}}"><i class="fa fa-angle-double-right"></i>Connect us</a></li>
+                        @guest
+                        <li><a href="{{url('/login')}}"><i class="fa fa-angle-double-right"></i>Login</a></li>
+                        <li><a href="{{url('/register')}}"><i class="fa fa-angle-double-right"></i>register</a></li> 
+                        @endguest
+
                     </ul>
                 </div>
                 <div class="col-xs-12 col-sm-4 col-md-4">
                     <h5>Category</h5>
                     <ul class="list-unstyled quick-links">
-                        <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Category</a></li>
-                        <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Category</a></li>
-                        <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Category</a></li>
-                        <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Category</a></li>
+                        <?php $categories=\App\Category::all();?>
+                        @foreach ($categories as $category)
+                            <li><a href="{{url('?category='. $category->name)}}"><i class="fa fa-angle-double-right"></i>{{$category->name}}</a></li>
+                        @endforeach
                     </ul>
                 </div>
                 <div class="col-xs-12 col-sm-4 col-md-4">
@@ -32,7 +35,7 @@
 
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-                    <p class="h6">&copy All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+                    <p class="h6">&copy All right Reversed.<a class="text-green ml-2" href="{{url('/')}}" target="_blank">{{config('app.name')}}</a></p>
                 </div>
                 </hr>
             </div>	
