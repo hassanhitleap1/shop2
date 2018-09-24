3a8061a61d6dfc4456100294de7c36015df82864
hasan <hassanhitleap1@gmail.com>
2018-09-24 17:02:38 +0300

    MAIN

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,27 +21,6 @@
       <!-- footer end -->
         @include('sections.modal')
       <!-- js model -->
-      <a data-toggle="modal" href="http://fiddle.jshell.net/Sherbrow/bHmRB/0/show/" data-target="#myModal">Click me !</a>
-
-      <!-- Modal -->
-      <div class="modal fade" id="remote-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-              <div class="modal-content">
-                  <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                       <h4 class="modal-title">Modal title</h4>
-      
-                  </div>
-                  <div class="modal-body"><div class="te"></div></div>
-                  <div class="modal-footer">
-                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-primary">Save changes</button>
-                  </div>
-              </div>
-              <!-- /.modal-content -->
-          </div>
-          <!-- /.modal-dialog -->
-      </div>
       <script>
          $(document).scroll(function(e){
                var scrollTop = $(document).scrollTop();
@@ -54,13 +33,7 @@
          $(document).ready(function () {
             $("#loginClick").click(function (e) { 
                 e.preventDefault();
-        // reset modal body with a spinner or empty content
-        spinner = "<div class='text-center'><i class='fa fa-spinner fa-spin fa-5x fa-fw'></i></div>"
-
-$("#remote-modal .modal-body").html(spinner)
-$("#remote-modal .modal-body").load(url);
-$("#remote-modal").modal("show");
-              //  $('#loginModal').modal('show');
+               $('#loginModal').modal('show');
             });
             $(".button-save").click(function (e) { 
                 e.preventDefault();
