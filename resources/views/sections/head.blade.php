74b328cbb5bf7798fae03260fcefa7261d1d668f
hasan <hassanhitleap1@gmail.com>
2018-09-25 15:36:16 +0300

    drop dawn js

--- resources/views/sections/head.blade.php
+++ resources/views/sections/head.blade.php
@@ -5,3 +5,4 @@
 <script src="{{asset('lib/jquery-3.3.1/jquery-3.3.1.slim.min.js')}}"></script>
 <script src="{{asset('lib/bootstrap-4.1.3/js/bootstrap.min.js')}}" ></script>
 <script src="{{asset('js/main.js')}}"></script>
+<script src="{{asset('lib/bootstrap-4.1.3/js/bootstrap.min.js')}}" ></script>
