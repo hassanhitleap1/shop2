d12223929078a82572c5c814959c70d23ab12375
hasan <hassanhitleap1@gmail.com>
2018-09-25 15:32:33 +0300

    on boot load category

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        view()->composer('index', function ($view) {
+            $categories=\App\Category::all();
+            $view->with('categories', $categories);
+        });
     }
 
     /**
