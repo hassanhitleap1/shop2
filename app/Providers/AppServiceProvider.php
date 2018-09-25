8ca58cc0fa4391e5595944b58e12a7e2b63281ec
hasan <hassanhitleap1@gmail.com>
2018-09-25 17:32:54 +0300

    residual from catgory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,12 +16,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        $categories=\App\Category::all();
-        view()->composer('index',function ($view) use ($categories){
-            $view->with('categories', $categories);
+        $categories=Category::all();
+        //skip 5 and take all 
+        $count = $categories->count();
+        $skip = 5;
+        $limit = $count - $skip; // the limit
+        $residual = Category::skip($skip)->take($limit)->get();
+        view()->composer('index',function ($view) use ($categories,$residual){
+            $view->with('categories', $categories)->with('residual',$residual);
         });
-        view()->composer('sections.nav',function ($view) use ($categories){
-            $view->with('categories', $categories);
+        view()->composer('sections.nav',function ($view) use ($categories,$residual){
+            $view->with('categories', $categories)->with('residual',$residual);
         });
     }
 
