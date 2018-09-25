afc69aa7d4b2b5e9c1320352565ba03d9a09d6da
hasan <hassanhitleap1@gmail.com>
2018-09-25 12:50:52 +0300

    add lib

--- resources/views/models/login.blade.php
+++ resources/views/models/login.blade.php
@@ -5,9 +5,8 @@
          <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
             @csrf
             <div class="form-group">
-               <label for="exampleInputEmail1">Email address</label>
-               <input type="text" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="email" value="{{ old('email') }}" required autofocus aria-describedby="emailHelp" placeholder="Enter email">
-               <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+               <label for="email">Email address</label>
+               <input type="text" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="email" value="{{ old('email') }}" required autofocus placeholder="Enter email">
                @if ($errors->has('email'))
                <span class="invalid-feedback" role="alert">
                <strong>{{ $errors->first('email') }}</strong>
@@ -15,7 +14,7 @@
                @endif
             </div>
             <div class="form-group">
-               <label for="exampleInputPassword1">Password</label>
+               <label for="password">Password</label>
                <input placeholder="Password" type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password" value="{{ old('password') }}" required >
                @if ($errors->has('password'))
                <span class="invalid-feedback" role="alert">
