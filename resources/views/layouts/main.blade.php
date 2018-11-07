5d257ad923d0edf51608ae6a853ab859ac14e13b
hasan <hassanhitleap1@gmail.com>
2018-11-07 08:06:08 +0300

    facebook Pixel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -48,16 +48,7 @@
                     $('#model').modal('show');   
                 });
             });
-            $('.animate').hover(function() {
-                    element = $(this);
-                $(element).addClass('animated bounce');
-                        window.setTimeout( function(){
-                            removeClassmm(element);
-                        }, 2000);  
-                });
-            function removeClassmm(element) {
-                $(element).removeClass( "animated bounce" ); 
-            }
+ 
          });
       </script>
       <!-- end js model -->
@@ -73,5 +64,23 @@
 
         gtag('config', 'UA-127102137-1');
         </script>
+        <!-- Facebook Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '180321722907815');
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=180321722907815&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Facebook Pixel Code -->
+
    </body>
 </html>
